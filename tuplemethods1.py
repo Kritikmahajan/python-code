f870e7f0e34a35da7636808f1af42f10989a934b
Kritikmahajan <kritikmahajan9@gmail.com>
2021-12-24 20:56:28 +0530

    adding a new code

---
+++ tuplemethods1.py
@@ -0,0 +1,22 @@
+# In this we will be discussing about the tuples methods.
+# A tuple is similar to a list but the only difference is that you cannot change anything in the tuple.
+# A tuple is denoted by round brackets().
+
+# let us start with taking a tuple.
+
+tuple=(5, 2, 6, 1, 9, 7, 3, 5)
+print(tuple)
+
+# Lets start with tuple methods.
+
+# 1. t.count(a)
+# This will count the number of times 'a' occurs in the tuple.
+
+print(tuple.count(5))
+
+# 2. t.index(b)
+#This will give the index of the first occurence of 'b'.
+
+print(tuple.index(6))
+
+# THANKYOU
